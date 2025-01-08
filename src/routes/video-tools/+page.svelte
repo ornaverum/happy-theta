0d325081b543219827cd6748fd81a01f70916a11
Jack Waddell <waddellj@asmsa.org>
2025-01-07 20:46:19 -0600

    Got Spring and Tween working with Svelte-Canvas.

--- src/routes/video-tools/+page.svelte
+++ src/routes/video-tools/+page.svelte
@@ -1,17 +1,31 @@
-<script>
+<script lang="ts">
     import { Canvas, Layer } from 'svelte-canvas';
+    import { onMount } from 'svelte';
+    import { Tween, Spring } from 'svelte/motion';
+    import { quadOut as easing } from 'svelte/easing';
+  
+    // let position = new Tween([0.5, 0.5], {easing});
+    let position = new Spring([0.5, 0.5]);
+    
+  
+    onMount(() => {
+      setInterval(() => 
+        (position.target = [Math.random(), Math.random()]), 
+        1000);
+    });
   
     const render = ({ context, width, height }) => {
-      context.font = `${width / 10}px sans-serif`;
+      const [x, y] = position.current;
       context.fillStyle = 'tomato';
-      context.textAlign = 'center';
-      context.textBaseline = 'top';
-      context.fillText('hello world', width / 2, height / 2);
+      context.beginPath();
+      context.arc(x * width, y * height, 20, 0, 2 * Math.PI);
+      context.fill();
     };
   </script>
   
-  <div class=''>
-      <Canvas width={600} height={600} class='mx-auto'>
+  <div class="mx-auto h-screen w-screen flex items-center justify-center">
+      <Canvas>
         <Layer {render} />
       </Canvas>
   </div>
+  
