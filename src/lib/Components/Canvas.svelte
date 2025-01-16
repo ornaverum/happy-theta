6d3288b137271701defc1ddf2cdd8ee059e24528
Jack Waddell <waddellj@asmsa.org>
2025-01-16 15:28:34 -0600

    idk

--- src/lib/Components/Canvas.svelte
+++ src/lib/Components/Canvas.svelte
@@ -1,28 +1,44 @@
 <script lang="ts">
     import {onMount} from 'svelte';
 
-
 	interface Props {
 		name?: string;
 		width?: number;
 		height?: number;
 		background?: string;
+		children?: any;
 	}
 
 	let {
 		name = 'Canvas',
 		width = 300,
 		height = 300,
-		background = '#fff'
+		background = '#fff',
+		children = []
 	}: Props = $props();
 	
-	let canvas = $state();
-	let context
+	let canvas:HTMLCanvasElement = $state();
+	let context: CanvasRenderingContext2D = $state();
 
+	onMount(() => {
+		context = canvas.getContext('2d');
+		context.fillStyle = background;
+		context.fillRect(0, 0, width, height);
+
+		console.log(children);
+	});
+
+	const getContext = () => {return context};
+
+	const addImage = (img:HTMLImageElement) => {
+		img.onload = () => {
+			context.drawImage(img, 0, 0);
+		};
+	};
 
 </script>
 
 
 <div>
-    <canvas bind:this={canvas} width={width} height={height} class='bg-primary-200'></canvas>
+    <canvas bind:this={canvas} width={width} height={height} class='bg-primary-200'/>
 </div>
