0bfdad1df7e6485b16285821e6db58a1810db3d6
Jack Waddell <waddellj@asmsa.org>
2025-08-13 13:37:53 -0500

    dunno

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-vercel';
+// import adapter from '@sveltejs/adapter-vercel';
+// import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
