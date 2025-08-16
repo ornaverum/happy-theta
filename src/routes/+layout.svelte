f3443c5b87d23c024c00a3a2bf500cc6cd011fe4
Jack Waddell <waddellj@asmsa.org>
2025-08-15 19:15:30 -0500

    Fixed gradient background

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,32 +18,31 @@
 <!-- <DarkMode btnClass={darkmodebtn} /> -->
  <!-- <DarkMode></DarkMode> -->
 
-<Navbar let:hidden let:toggle >
-	<NavBrand href="/">
-		<object data="/HappyThetaLogo.svg" class="max-h-12" title="Happy Theta Logo"></object>
-		<!-- <object data="/HappyThetaLogo.svg" class="me-3 h-6 sm:h-9" title="Happy Theta Logo"></object> -->
-		<!-- <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Inertial Apple</span> -->
-
-	</NavBrand>
-	<NavHamburger class='ml-16' />
-	<NavUl {hidden}>
-	  <NavLi href="{base}/">Home</NavLi>
-	  <NavLi href="{base}/figures/free-body-diagram">Free Body Diagram</NavLi>
-	  <NavLi href="{base}/figures/energy-diagram">Energy Diagram</NavLi>
-	  <NavLi href="{base}/figures/motion-diagram">Motion Diagram</NavLi>
-	  <NavLi href="{base}/figures/graph-maker">Graph Maker</NavLi>
-	  <!-- <NavLi href="{base}/grapher">Grapher</NavLi> -->
-	  <!-- <NavLi href="{base}/vid-analysis">Video Analysis</NavLi> -->
-	  <!-- <NavLi href="{base}/equation-editor">Equation Editor</NavLi> -->
-	</NavUl>
-  </Navbar>
-<div class="h-auto bg-gradient-to-b from-white to-blue-200 ">
+<div class="h-screen max-w-full overflow-x-hidden bg-gradient-to-b from-white to-blue-200">
+	<Navbar let:hidden let:toggle >
+		<NavBrand href="/">
+			<object data="/HappyThetaLogo.svg" class="max-h-12" title="Happy Theta Logo"></object>
+			<!-- <object data="/HappyThetaLogo.svg" class="me-3 h-6 sm:h-9" title="Happy Theta Logo"></object> -->
+			<!-- <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Inertial Apple</span> -->
 	
-	{@render children?.()}
-	
-</div>
-<div class='h-full bg-blue-200'>
-
+		</NavBrand>
+		<NavHamburger class='ml-16' />
+		<NavUl {hidden}>
+		  <NavLi href="{base}/">Home</NavLi>
+		  <NavLi href="{base}/figures/free-body-diagram">Free Body Diagram</NavLi>
+		  <NavLi href="{base}/figures/energy-diagram">Energy Diagram</NavLi>
+		  <NavLi href="{base}/figures/motion-diagram">Motion Diagram</NavLi>
+		  <NavLi href="{base}/figures/graph-maker">Graph Maker</NavLi>
+		  <!-- <NavLi href="{base}/grapher">Grapher</NavLi> -->
+		  <!-- <NavLi href="{base}/vid-analysis">Video Analysis</NavLi> -->
+		  <!-- <NavLi href="{base}/equation-editor">Equation Editor</NavLi> -->
+		</NavUl>
+	  </Navbar>
+	<div class="">
+		{@render children?.()}
+	</div>
+	<div class=''>
+	</div>
 </div>
 <!-- 
 <Footer class="fixed bottom-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,8 @@
 
 </script>
 
-<div class='w-full h-full min-h-96'>
-	<Card class='bg-green mx-auto w-max my-20'>
+<div class=''>
+	<Card class='bg-gray-100 mx-auto w-max my-20'>
 		<h1 class="text-3xl dark:text-white w-max">Welcome to Happy Theta</h1>
 		<p class="dark:text-white my-4">
 			Physics tools for students and teachers. Still under development.
