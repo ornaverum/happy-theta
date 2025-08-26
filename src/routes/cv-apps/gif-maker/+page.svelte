c4f9feabcb995fc483602ca8de3668118995d9fe
Jack Waddell <waddellj@asmsa.org>
2025-08-26 17:21:35 -0500

    Now displays chosen image and image list. Need to standardize sizing. Need to put main image on a canvas instead of img.

--- src/routes/cv-apps/gif-maker/+page.svelte
+++ src/routes/cv-apps/gif-maker/+page.svelte
@@ -17,66 +17,57 @@
         : null
     );
 
-    $inspect(fileNames);
-    let selectedFileIndex:number = $state(0);
 
-    let mainViewImage:HTMLImageElement | undefined = $state(undefined);
-    let mainViewCanvasImage: CanvasImageSource|undefined = $state(undefined);
-
-    const onMainViewImageChange = (i:number = 0) => {
-        if (fileNames){
-            mainViewImage = new window.Image();
-            mainViewImage.src = fileNames[i];
-            mainViewImage.onload = () => mainViewCanvasImage = mainViewImage;
-        } else {
-            mainViewImage = undefined;
+    $effect (()=>{
+        if (selectedFiles) {
+            $inspect(URL.createObjectURL(selectedFiles[selectedFileIndex]));
         }
-    };
+    });
 
-    let mainViewCanvas: HTMLCanvasElement | undefined = $state(undefined);
-    let mainViewContext: CanvasRenderingContext2D | undefined = $derived(
-        () => mainViewCanvas?.getContext("2d")
-    );
+    // $inspect(URL.createObjectURL(selectedFiles[selectedFileIndex]));
+    let selectedFileIndex:number = $state(0);
+   
 
 </script>
 
 
 <div>
-    <div id='gif' class='mx-auto w-4/5 flex flex-col bg-gray-100 w-full rounded-xl shadow-lg items-center'>
+    <div id='gif' class='mx-auto w-4/5 flex flex-col bg-gray-100 rounded-xl shadow-lg items-center'>
         <div id='gif-label' class='p-3 ml-4 text-lg font-bold'>
             GIF Maker
         </div>
         
-        <div class='flex flex-row bg-green-200 p-3 m-3 overflow-y-scroll'>
-            <div id='main-view' class='min-w-40 w-4/5 bg-slate-200 '>
-                {#if fileNames}
-                    <canvas bind:this={mainViewCanvas}>
-
-                    </canvas>
-
+        
+        <div class='flex flex-row bg-green-200 p-3 m-3 '>
+            <div id='main-view' class='min-w-40 max-w-4/5  bg-slate-200 p-4'>
+                {#if selectedFiles}
+                    <img src={URL.createObjectURL(selectedFiles[selectedFileIndex])} alt='Selected Image' class='max-w-full max-h-96'/>
                 {:else}
-
-                {/if}
-                
-                
-            </div>
-            <div class='flex flex-col max-h-xl'>
-                {#each [1, 2, 3, 4] as i}
-                    <div>               
-                        <Stage width={100} height={100} style="border: 1px solid black; background-color: white;">
-                            <Layer>
-                                <Rect x={10} y={10} width={80} height={80} fill="blue" />   
-                            </Layer>
-                        </Stage>
+                    <div class='w-full h-40 bg-gray-300 flex items-center justify-center text-gray-500'>
+                        No image selected
                     </div>
-                {/each}
+                {/if}
+            </div>
+            <div class='flex flex-col max-h-80 p-2 space-y-2 overflow-y-scroll'>
+                {#if selectedFiles && selectedFiles.length > 1}
+                    <div class='text-sm font-semibold'>Select Image:</div>
+                    {#each Array.from(selectedFiles) as file, index}
+                        <button 
+                            class="px-2 py-1 text-xs rounded {selectedFileIndex === index ? 'bg-blue-500 text-white' : 'bg-gray-200 hover:bg-gray-300'}"
+                            onclick={() => selectedFileIndex = index}
+                        >
+                            <img src={URL.createObjectURL(selectedFiles[index])} alt='Selected Image' class='max-w-full max-h-10 object-contain'/>
+
+                        </button>
+                    {/each}
+                {/if}
             </div>
             
         </div>
         <div class='w-1/2 m-4'>
-            <Label class="p-2" for="image_files">Upload image files</Label>
-                <Fileupload clearable bind:files={selectedFiles} multiple />
-                <Helper class="mt-2">Selected files: {fileNames ? fileNames : "No files selected"}</Helper>
+            <Label class="p-2" for="image_files" >Upload image files</Label>
+            <Fileupload clearable bind:files={selectedFiles} multiple  onchange={(e) => {console.log(e.target.files)}}/>
+            <Helper class="mt-2">Selected files: {fileNames ? fileNames : "No files selected"}</Helper>
         </div>
         
 
