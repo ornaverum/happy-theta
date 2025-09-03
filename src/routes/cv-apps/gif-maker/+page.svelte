3b51f5d1d5722d24bd122f002ca776d8985155b6
Jack Waddell <waddellj@asmsa.org>
2025-09-03 14:07:20 -0500

    added duration slider and title input to gif-maker

--- src/routes/cv-apps/gif-maker/+page.svelte
+++ src/routes/cv-apps/gif-maker/+page.svelte
@@ -2,7 +2,7 @@
 <script lang="ts">
     import { Stage, Layer, Rect, Image as KonvaImage } from 'svelte-konva';
     import { onMount, tick } from 'svelte';
-    import {Fileupload, Helper, Label, Button, type ImgType} from 'flowbite-svelte';
+    import {Fileupload, Helper, Label, Button, type ImgType, Range,} from 'flowbite-svelte';
     import cv from '@techstark/opencv-js'
     import gifshot from 'gifshot';
 
@@ -71,6 +71,7 @@
             return;
         }
   
+        gifLoading = true;
         // Convert files to dataURLs
         const readFileAsDataURL = (file: File): Promise<string> => {
             return new Promise((resolve, reject) => {
@@ -91,8 +92,8 @@
                 'images': dataURLs,
                 'gifWidth': 500,
                 'gifHeight': h/w*500,
-                'frameDuration': 10,
-                'text': 'boo',
+                'frameDuration': 10*frameDelay,
+                'text': gifTitle,
                 'textAlign': 'right',
                 'fontColor': '#ff9922',
                 'fontSize': '24px',
@@ -108,8 +109,21 @@
         } catch (error) {
             console.error("Error reading files:", error);
         }
+        gifLoading = false;
     };
 
+
+    const clearAll = () => {
+        selectedFiles = undefined;
+        fileListForUpload = undefined;
+        fileObjectURLs = [];
+        dataURLs = [];
+        selectedFileIndex = 0;
+        const gifPreviewDiv = document.getElementById('gif-preview');
+        if (gifPreviewDiv) {
+            gifPreviewDiv.innerHTML = '';
+        }
+    };
     const removeFile = (index: number) => {
         if (selectedFiles) {
             const filesArray = [...selectedFiles]; // Create a copy of the array
@@ -133,10 +147,16 @@
 
     let mainViewDiv: HTMLDivElement | null = null;
 
+    let frameDelay:number = $state(1);
+    let gifTitle:string = $state("My GIF");
+
+    let gifLoading: boolean = $state(false);
+
     const clearFiles = () => {
         selectedFiles = undefined;
         fileObjectURLs = [];
         selectedFileIndex = 0;
+        gifLoading = false;
     };
 
     const saveGif = () => {
@@ -196,19 +216,32 @@
             </div>
             
         </div>
-        <div id='gif-preview' class='bg-emerald-100 mx-auto'>
-        </div>
+
         <div class='w-1/2 m-4'>
             <Label class="p-2" for="image_files" >Upload image files</Label>
             <Fileupload class="bg-white text-white" clearable bind:files={fileListForUpload} 
                 multiple accept='.jpg, .jpeg, .heic, .png'  onchange={(e) => {console.log((e.target as HTMLInputElement)?.files)}}/>
             <Helper class="mt-2">Selected files: {fileNames ? fileNames : "No files selected"}</Helper>
+            <label for="slider">Select frame duration (seconds): {frameDelay}</label><br />
+            <input type="range" id="slider" min="0.1" max="1" step="0.1" bind:value={frameDelay} class='w-full'/>
+            <div class='w-full flex flex-row'>
+                <label for='gifTitle' class='pr-4 my-auto'>GIF Title:</label>
+                <input id='gifTitle' type='text' bind:value={gifTitle} class='grow border border-gray-300 rounded px-2' />
+
+            </div>
+
+            <div id='gif-preview' class='mx-auto py-3'>
+                {#if gifLoading}
+                    <div>Loading...</div>
+                {/if}
+            </div>
             <div class='flex space-x-2'>
                 <Button disabled={!selectedFiles || selectedFiles.length === 0} class="mt-4 px-4 py-2 bg-blue-500 text-white rounded" onclick={handleMakeGif}>
                     Create GIF from Images
                 </Button>
 
                 <Button disabled={!selectedFiles || selectedFiles.length === 0}  class="mt-4 px-4 py-2 bg-blue-500 text-white rounded" onclick={saveGif}>Save Gif</Button>
+                <Button class="mt-4 px-4 py-2 bg-red-500 text-white rounded" onclick={clearAll}>Reset</Button>
             </div>
         </div>
     </div> 
