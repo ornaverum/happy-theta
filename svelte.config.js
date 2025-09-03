d554c392bad0d2b8462e2bf5d241e229fa363541
Jack Waddell <waddellj@asmsa.org>
2025-09-02 19:15:04 -0500

    adapter change

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,8 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-vercel';
+// import adapter from '@sveltejs/adapter-vercel';
+// import adapter from '@sveltejs/adapter-auto';
 // import adapter from '@sveltejs/adapter-node';
-// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
